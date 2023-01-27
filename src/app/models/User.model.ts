693d0096cde91fe297a764f43b594085dd670aee
HMemel <memelmegame@gmail.com>
2023-01-27 02:45:05 +0000

    First Commit

---
+++ src/app/models/User.model.ts
@@ -0,0 +1,14 @@
+export class User {
+  constructor(public name: string,
+              public prenom: string,
+              public email: string,
+              public profilePictureUrl: string
+             ){}
+}
+
+
+
+
+
+
+
